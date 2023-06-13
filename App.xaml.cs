88fda9ea11edad2c9b88767690cfdc86be8fb98f
Kristóf Kerekes <kristof.kerekes@shapr3d.com>
2023-06-13 08:12:44 +0200

    Separate Tray from MainWindow + Handle states in App

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,10 +5,44 @@ namespace ACCWindowManager {
 		public App() {
 			m_windowController = new ACCWindowController();
 
-			MainWindow = new MainWindow(new MainWindowViewModel(m_windowController));
-			MainWindow.Show();
+			m_TrayIconWindow = new TrayIconWindow();
+			m_TrayIconWindow.OpenRequested += OnOpenRequested;
+			m_TrayIconWindow.ExitRequested += OnExitRequested;
+
+
+			MainWindow = m_TrayIconWindow;
+			if (!ACCWindowManager.Properties.Settings.Default.WasOnTray) {
+				OpenMainWindow();
+			}
 
 			m_windowController.Initialize();
+		}
+
+		private void OpenMainWindow() {
+			m_mainWindow = new MainWindow(new MainWindowViewModel(m_windowController));
+			m_mainWindow.MinimizeToTrayRequested += OnMinimizeToTrayRequested;
+			m_mainWindow.Show();
+		}
+
+		private void OnOpenRequested() {
+			if (m_mainWindow == null) {
+				OpenMainWindow();
+			}
+			m_mainWindow.Activate();
+
+			ACCWindowManager.Properties.Settings.Default.WasOnTray = false;
+			SettingsSaveRequested();
+		}
+
+		private void OnExitRequested() {
+			Shutdown();
+		}
+
+		private void OnMinimizeToTrayRequested() {
+			m_mainWindow = null;
+
+			ACCWindowManager.Properties.Settings.Default.WasOnTray = true;
+			SettingsSaveRequested();
 		}
 
 		public static void SettingsSaveRequested() {
@@ -20,5 +54,7 @@ namespace ACCWindowManager {
 		}
 
 		ACCWindowController m_windowController;
+		TrayIconWindow m_TrayIconWindow;
+		MainWindow m_mainWindow;
 	}
 }
